1749adc7a2f33edd6209a004293dec875cd45630
Sabrina Kessar <sabbendra@gmail.com>
2022-09-13 10:02:20 +0200

    recup backend

---
+++ backend/server.js
@@ -0,0 +1,10 @@
+require("dotenv").config()
+const express = require("express")
+const app = express()
+const cors = require("cors")
+
+//middleware 
+app.use(cors())
+app.use(express.json())
+
+module.exports = { app, express }
