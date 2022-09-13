53d43245bfff757aaf72fdaf7f0770affd232886
Sabrina Kessar <sabbendra@gmail.com>
2022-09-13 10:05:02 +0200

    recup backend

---
+++ backend/index.js
@@ -0,0 +1,32 @@
+const {app, express} = require("./server") //on importe app et express
+const port = 3000
+const path = require("path")//permet de cloner deux chemins ensemble
+
+//connection à datebase
+require("./mongo") // on a besoin de lancer le fichier mongo
+
+//Controllers, les contrôles
+const { createUser, logUser } = require("./controllers/users")
+const { getSauces, createSauces, getSauceById } = require("./controllers/sauces")
+
+//middleware
+const { upload }  = require("./middleware/multer")
+const { authenticateUser } = require("./middleware/auth")
+
+//routes
+app.post("/api/auth/signup", createUser) 
+app.post("/api/auth/login", logUser)
+//authenticateUser est un middlew il se loge entre la requête et la réponse
+app.get("/api/sauces", authenticateUser, getSauces)
+//apres l'authentification, on va récupérer un fichier image et ensuite on va créer une sauce
+app.post("/api/sauces", authenticateUser, upload.single("images"), createSauces)
+app.get("/api/sauces/:id", authenticateUser, getSauceById)
+
+app.get("/", (req, res) => res.send("hello world"))
+
+//Listen
+console.log(__dirname)
+console.log(path.join(__dirname,"images"))
+
+app.use("/images", express.static(path.join(__dirname, "images")))
+app.listen(port, () => console.log('Listening on port' + port))
---
+++ backend/mongo.js
@@ -0,0 +1,26 @@
+//Database
+const mongoose = require('mongoose')
+const uniqueValidator = require("mongoose-unique-validator")
+const password = process.env.DB_PASSWORD
+const username = process.env.DB_USER
+const db = process.env.DB_NAME
+
+const uri = `mongodb+srv://${username}:${password}@cluster0.hl6gvhp.mongodb.net/${db}?retryWrites=true&w=majority`
+
+
+
+mongoose
+.connect(uri)
+.then(() => console.log("Connexion à MongoDB réussie !"))
+.catch(() => console.log("Connexion à MongoDB échouée !"));
+
+const userSchema = new mongoose.Schema({
+  email: {type: String, required: true, unique: true},
+  password: {type: String, required: true}
+})
+userSchema.plugin(uniqueValidator)
+
+const User = mongoose.model("User", userSchema)
+
+//c'est pour exporter mongoose et User
+module.exports = {mongoose, User}
