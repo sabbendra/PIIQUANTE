8112fc40e2b31a98c9c01d7ecb1e3c4a12de3fa1
Sabrina Kessar <sabbendra@gmail.com>
2022-09-13 10:08:04 +0200

    recup backend

---
+++ backend/controllers/sauces.js
@@ -0,0 +1,65 @@
+//pour vérifier le token
+const mongoose = require("mongoose")
+
+const productSchema = new mongoose.Schema({
+    userId: { type: String, required: true },
+    name: { type: String, required: true },
+    manufacturer: { type: String, required: true },
+    description: { type: String, required: true },
+    mainPepper: { type: String, required: true },
+    imageUrl: { type: String, required: true }, 
+    heat: { type: Number, required: true },
+    likes: { type: Number, required: true },
+    dislikes: { type: Number },
+    userLiked: [String],
+    userDisliked: [String]
+})
+
+const Product = mongoose.model("Product", productSchema)
+
+function getSauces(req, res){
+        Product.find({})
+        .then((Product) => res.send(Product))
+        .catch(error => res.status(500).send(error)) 
+        
+}
+
+function getSauceById(req, res) { 
+    const { id } = req.params
+    Product.findOne({id}) 
+        .then((Product) => res.send(Product))
+        .catch(console.error)               
+}
+
+//pour créer une sauce, res c'est la réponse qu'on reçoit
+function createSauces(req, res){
+const { body, file } = req
+const { fileName } = file
+const sauce = JSON.parse(body.sauce)
+const { name, manufacturer, description, mainPepper, heat, userId } = sauce
+
+function makeImageUrl(req, fileName) {
+    return req.protocol + "://" + req.get("host") + "/images/" + fileName
+}
+ const product = new Product({
+    userId: userId,
+    name: name,
+    manufacturer: manufacturer,
+    description: description,
+    mainPepper: mainPepper, 
+    imageUrl: makeImageUrl(req, fileName),
+    heat: heat,
+    likes: 0,
+    dislikes: 0,
+    userLiked: [],
+    userDisliked: []
+ })
+ product
+ .save()
+ .then((message) =>  {
+    res.status(201).send( message );
+    return console.log("produit enregisté", message)
+ })
+ .catch(console.error)
+}
+module.exports = { getSauces, createSauces, getSauceById }
---
+++ backend/controllers/users.js
@@ -0,0 +1,52 @@
+const {User} = require ("../mongo")//on veut le User dans le fichier mongo
+const bcrypt = require("bcrypt")
+const jwt = require ("jsonwebtoken")
+
+async function createUser(req, res) {
+    try {
+    const { email, password } = req.body
+    const hashedPassword = await hashPassword(password)
+    console.log("password:", password)
+    console.log("hashedPassword:", hashedPassword)
+
+    const user = new User({ email, password: hashedPassword })
+    await user.save()
+        res.status(201).send({message: "Utilisateur enregistré !"})
+    } catch (err) {
+        res.status(409).send({message: "User non enregistré :" + err})
+    }
+  }
+
+  function hashPassword(password){
+    const saltRounds = 10;
+    return bcrypt.hash(password, saltRounds)
+  }
+
+  async function logUser(req, res) {
+   
+    try {
+    const email = req.body.email
+    const password = req.body.password
+    
+    //pour trouver une personne dans la base de donnée
+    const user = await User.findOne({ email: email })
+    console.log(user)
+    const isPasswordOk = await bcrypt.compare(password, user.password)
+    //si le password n'est pas ok on envoie un 403
+    if(!isPasswordOk){
+        res.status(403).send({ message: "Mot de passe incorrect"})
+    }
+    const token = createToken(email)
+    res.status(200).send({ userId: user?._id, token: token})    
+  } catch (err) {
+    console.error(err)
+    res.status(500).send({ message: "Erreur interne" })
+  }
+}
+
+  function createToken(email) {
+    const jwtPassword = process.env.JWT_PASSWORD
+    return jwt.sign({ email: email }, jwtPassword, { expiresIn: "24h"})
+  }
+
+  module.exports = { createUser, logUser }
